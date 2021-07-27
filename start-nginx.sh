7b75e11af38e72ddd244fd0b1d8ef6af1861fe44
yonexbat <yonexbat@gmail.com>
2021-07-27 07:36:22 +0200

    next try

--- start-nginx.sh
+++ start-nginx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo 'starting nginx our own way!'
-sed -i "s|BASEPATH|$PathBase|" index.html
+sed -i "s|<base href=\"/\">|<base href=\"/$PathBase/\">|" index.html
 mkdir -p $PathBase
 cp ./* ./$PathBase
 nginx -g 'daemon off;'
